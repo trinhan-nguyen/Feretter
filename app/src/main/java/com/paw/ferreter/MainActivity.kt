9d42967f8a2ae7bf2871a980548749723928219f
Tri Nhan Nguyen <ngtrnhan1205@gmail.com>
2021-01-04 00:36:15 -0500

    Set Up UI For Accounts Page: enable data binding

--- app/src/main/java/com/paw/ferreter/MainActivity.kt
+++ app/src/main/java/com/paw/ferreter/MainActivity.kt
@@ -2,10 +2,15 @@ package com.paw.ferreter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.paw.ferreter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
